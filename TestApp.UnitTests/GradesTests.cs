1a9b4ea1ae04645559a924fff7494bc9eda8c5dd
Lastik30Bona <mhristov25@gmail.com>
2024-10-26 13:13:17 +0300

    Add project files.

---
+++ TestApp.UnitTests/GradesTests.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class GradesTests
+{
+    [TestCase(2.00, "Fail")]
+    [TestCase(2.99, "Fail")]
+    [TestCase(3.00, "Average")]
+    [TestCase(3.20, "Average")]
+    [TestCase(3.80, "Good")]
+    [TestCase(4.50, "Very Good")]
+    [TestCase(5.80, "Excellent")]
+    public void Test_GradeAsWords_ReturnsCorrectString(double grade, string expected)
+    {
+        // Act
+        string actual = Grades.GradeAsWords(grade);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+
+    [TestCase(1.00, "Invalid!")]
+    [TestCase(1.99, "Invalid!")]
+    [TestCase(1.01, "Invalid!")]
+    [TestCase(15.00, "Invalid!")]
+    public void Test_GradeAsWords_ReturnsCorrectString_EdgeCases(double grade, string expected)
+    {
+        // Act
+        string actual = Grades.GradeAsWords(grade);
+
+        // Assert
+        Assert.AreEqual(expected, actual);
+    }
+}
---
+++ TestApp.UnitTests/StringReverseTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+
+namespace TestApp.UnitTests;
+
+public class StringReverseTests
+{
+    // TODO: finish test
+    [Test]
+    public void Test_Reverse_WhenGivenEmptyString_ReturnsEmptyString()
+    {
+        // Arrange
+        string input = "";
+        string expected = "";
+
+        // Act
+        string result = StringReverse.Reverse(input);
+
+        // Assert
+        Assert.AreEqual(result, expected);
+    }
+
+    [Test]
+    public void Test_Reverse_WhenGivenSingleCharacterString_ReturnsSameCharacter()
+    {
+        // Arrange
+        string input = "S";
+        string expected = "S";
+
+        // Act
+        string result = StringReverse.Reverse(input);
+
+        // Assert
+        Assert.AreEqual(result, expected);
+    }
+
+    [Test]
+    public void Test_Reverse_WhenGivenNormalString_ReturnsReversedString()
+    {
+        // Arrange
+        string input = "abc";
+        string expected = "cba";
+
+        // Act
+        string result = StringReverse.Reverse(input);
+
+        // Assert
+        Assert.AreEqual(result, expected);
+    }
+}
